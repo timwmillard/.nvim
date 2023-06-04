82ce3404d9d9b0feb4e6ab436d403f95fc7d23db
Tim Millard <tim@rapidpaylegal.com>
2023-06-04 13:40:05 +1000

    lsp cmp confirm config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,22 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
+local lspconfig = require('lspconfig')
+
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select_opts),
+  }
+})
+
+lspconfig.clangd.setup{
+}
