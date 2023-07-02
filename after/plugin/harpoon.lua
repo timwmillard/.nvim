8a2c8c87902918e33ef3ea1d9b9fbe4249dabb1d
Tim Millard <tim@rapidpaylegal.com>
2023-07-02 22:49:52 +1000

    Better harpoon mappings - Lualine relative path

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,9 +3,9 @@ local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
 vim.keymap.set('n', '<leader>a', mark.add_file)
-vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu)
-vim.keymap.set('n', '<leader>hn', ui.nav_next)
-vim.keymap.set('n', '<leader>hp', ui.nav_next)
+vim.keymap.set('n', '<leader>hp', ui.toggle_quick_menu)
+vim.keymap.set('n', '<C-k>', ui.nav_prev)
+vim.keymap.set('n', '<C-j>', ui.nav_next)
 
 require("telescope").load_extension('harpoon')
 vim.keymap.set('n', '<leader>fh', ':Telescope harpoon marks<CR>')
