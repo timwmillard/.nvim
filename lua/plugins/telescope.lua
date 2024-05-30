8d4c368389d3ac4676089ecded643b19fd33c406
Tim Millard <timwmillard@gmail.com>
2024-05-31 09:19:09 +1000

    Space

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' },
+
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>p', builtin.find_files, {})
