9aa65965833223404f526e22259a3ae3d9aeadf9
Tim Millard <timwmillard@gmail.com>
2024-05-31 10:27:37 +1000

    Lazy vim-reset-console

---
+++ lua/plugins/vim-reset-console.lua
@@ -0,0 +1,11 @@
+-- Vim REST Console (VRC)
+return {    
+    'diepm/vim-rest-console',
+    config = function()
+        vim.g.vrc_output_buffer_name = '_OUTPUT.json'
+        vim.g.vrc_auto_format_response_patters = {
+            json = 'jq',
+        }
+    end
+}
+
