137f3e042158616d92a4f3dc3e12c276be52a383
Tim Millard <timwmillard@gmail.com>
2024-05-31 19:45:28 +1000

    Spacing

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -6,11 +6,11 @@ return {
             -- To get fugitive :GBrowse to work with nvim-tree
             -- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
             vim.api.nvim_create_user_command(
-              'Browse',
-              function (opts)
+            'Browse',
+            function (opts)
                 vim.fn.system { 'open', opts.fargs[1] }
-              end,
-              { nargs = 1 }
+            end,
+            { nargs = 1 }
             )
 
         end
