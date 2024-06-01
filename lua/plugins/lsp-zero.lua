b1fb1682f79da1163f302dfb8f63ee0edd314aa3
Tim Millard <timwmillard@gmail.com>
2024-06-01 22:34:18 +1000

    lsp-zero setup

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,40 @@
+return {
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {
+        'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+
+            lsp_zero.on_attach(function(_, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+
+            require("mason").setup({
+                 ui = {
+                     icons = {
+                         package_installed = "✓",
+                         package_pending = "➜",
+                         package_uninstalled = "✗"
+                     }
+                 }
+             })
+
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+              },
+            })
+        end
+    },
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    -- {'L3MON4D3/LuaSnip'},
+}
