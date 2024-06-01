31ea769037d240798db6afeafc8a6c706fa5ed11
Tim Millard <timwmillard@gmail.com>
2024-06-02 00:00:33 +1000

    More Lsp config

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,6 +1,10 @@
 return {
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
@@ -32,10 +36,62 @@ return {
                     end,
                 },
             })
-        end
+
+            local lspconfig = require('lspconfig')
+            -- (Optional) Configure lua language server for neovim
+            -- lspconfig.lua_ls.setup(lsp_zero.nvim_lua_ls())
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {
+                                'vim',
+                                'require'
+                            },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            })
+
+            vim.keymap.set('n', '<leader>fd', function()
+                vim.lsp.buf.format()
+            end, {})
+
+            vim.api.nvim_set_hl(0, '@lsp.type.comment.cpp', {})
+            vim.api.nvim_set_hl(0, '@lsp.type.comment.c', {})
+
+            local cmp = require('cmp')
+            cmp.setup({
+                mapping = {
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-k>'] = cmp.mapping.select_prev_item({}),
+                    ['<C-j>'] = cmp.mapping.select_next_item({}),
+                },
+            })
+
+            vim.lsp.handlers["textDocument/publishDiagnostics"] =
+                vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+                    -- Disable underline, it's very annoying
+                    underline = false,
+                    -- Enable virtual text, override spacing to 4
+                    virtual_text = { spacing = 4 },
+                    signs = true,
+                    update_in_insert = false
+                })
+        end -- function config
     },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    -- {'L3MON4D3/LuaSnip'},
 }
