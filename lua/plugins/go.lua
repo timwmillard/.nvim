e7b0b64a4c4a133bd6d95ff47625860cd4f3f7e0
Tim Millard <timwmillard@gmail.com>
2024-06-02 21:04:08 +1000

    feat: gofmt & goimport on save

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -6,7 +6,18 @@ return {
         "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-        require("go").setup()
+        require("go").setup({
+            gofmt = 'gofmt',
+        })
+
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                require('go.format').goimports()
+            end,
+            group = format_sync_grp,
+        })
     end,
     event = { "CmdlineEnter" },
     ft = { "go", 'gomod' },
