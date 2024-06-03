1fd3534305ae2c209a4b8058a817d30ced83b25f
Tim Millard <timwmillard@gmail.com>
2024-06-03 10:40:12 +1000

    Lazy dap plugins

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,9 @@
+return {
+    'mfussenegger/nvim-dap',
+    'theHamsta/nvim-dap-virtual-text',
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+    },
+    -- 'nvim-telescope/telescope-dap.nvim',
+}
