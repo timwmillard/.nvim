0425b3931bef35b9a161e2b795fb4ed7d0d00e7a
Tim Millard <timwmillard@gmail.com>
2024-06-03 14:25:09 +1000

    Telescope for dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,7 +3,10 @@ return {
     'theHamsta/nvim-dap-virtual-text',
     {
         "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            require('telescope').load_extension('dap')
+        end
     },
-    -- 'nvim-telescope/telescope-dap.nvim',
+    'nvim-telescope/telescope-dap.nvim',
 }
