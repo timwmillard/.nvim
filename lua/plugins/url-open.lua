a5b83ef93f4878f58281872a8686b2dc98c83379
Tim Millard <timwmillard@gmail.com>
2024-06-03 10:45:17 +1000

    fix(url-open): setup not being called with opts

--- lua/plugins/url-open.lua
+++ lua/plugins/url-open.lua
@@ -68,11 +68,6 @@ return {
                 -- so the url will be https://www.npmjs.com/package/[pattern_found]/issues
             },
         })
-        local status_ok, url_open = pcall(require, 'url-open')
-        if not status_ok then
-            return
-        end
-        url_open.setup({})
 
         vim.keymap.set("n", "<leader>u", "<esc>:URLOpenUnderCursor<cr>")
     end
