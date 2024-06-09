5d4dfccbf5aacb1f773331dbbf13bd38052f8929
Tim Millard <timwmillard@gmail.com>
2024-06-09 15:32:08 +1000

    Upgrade telescope to 0.1.8

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- File Search Fuzzy Finder
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
