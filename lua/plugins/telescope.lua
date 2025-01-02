607f6775b7f6948d671b80532e77b4047024075d
Tim Millard <timwmillard@gmail.com>
2025-01-02 15:58:29 +1100

    updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
 
         vim.keymap.set('n', '<leader>a', function()
             builtin.find_files({ hidden = true, no_ignore = true })
