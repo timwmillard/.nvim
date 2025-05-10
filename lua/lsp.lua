19a4b2b73bfe2702cf455148369452a594104b37
Tim Millard <timwmillard@gmail.com>
2025-05-10 22:33:37 +1000

    improve completion

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,31 +1,31 @@
 local on_attach = function(client, bufnr)
-  -- Enable keybindings only for buffers with LSP attached
-  local opts = { noremap=true, silent=true, buffer=bufnr }
-
-  -- Go to definition with gd
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-
-  -- Other useful LSP keybindings
-  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
+
+-- https://blog.viktomas.com/graph/neovim-native-built-in-lsp-autocomplete/
+-- https://neovim.io/doc/user/options.html#'completeopt'
+vim.opt.completeopt = {'menuone', 'noselect', 'popup', 'fuzzy'}
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        if client ~= nil and client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf,
+                {
+                    autotrigger = true,
+                    convert = function(item)
+                        return { abbr = item.label:gsub("%b()", "") }
+                    end,
+                }
+            )
         end
 
-        on_attach(client, ev.buf)
+        vim.keymap.set("i", "<C-space>", vim.lsp.completion.get,
+            { desc = "trigger autocompletion" }
+        )
 
-        -- -- Unset 'formatexpr'
-        -- vim.bo[ev.buf].formatexpr = nil
-        -- -- Unset 'omnifunc'
-        -- vim.bo[ev.buf].omnifunc = nil
-        -- -- Unmap K
-        -- vim.keymap.del('n', 'K', { buffer = ev.buf })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition,
+            { noremap=true, silent=true, buffer=ev.buf }
+        )
     end
 })
 
