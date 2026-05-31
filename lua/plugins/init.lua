775f03d460f80623ae866559b7bef4d1f8554378
Tim Millard <timwmillard@gmail.com>
2026-06-01 09:18:37 +1000

    restructure init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,4 @@
 return {
-    -- {
-    --     -- Undo Tree
-    --     'mbbill/undotree',
-    --     config = function()
-    --         vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle)
-    --     end
-    -- },
     {
         'ThePrimeagen/vim-be-good'
     },
@@ -15,41 +8,26 @@ return {
         dir = '~/dev/uuid.nvim',
         config = function() require('uuid') end
     },
-    -- {
-    --     -- HTML snippets and shortcuts
-    --     -- https://raw.githubusercontent.com/mattn/emmet-vim/master/TUTORIAL
-    --     'mattn/emmet-vim'
-    -- },
     {
         -- Hex view for binary files
         'RaafatTurki/hex.nvim',
         config = function() require('hex').setup() end
     },
-    -- {
-    --     'kwakzalver/duckytype.nvim',
-    --     config = function()
-    --         require('duckytype').setup({})
-    --     end
-    -- },
-    -- {
-    --     "ledger/vim-ledger",
-    -- },
-    -- {
-    --     "AndrewRadev/linediff.vim"
-    -- },
     {
         "nvzone/minty",
         dependencies = { "nvzone/volt", lazy = true },
         cmd = { "Shades", "Huefy" },
     },
-    -- {
-    --     "nvzone/showkeys",
-    --     cmd = "ShowkeysToggle",
-    --     opts = { position = "top-center" },
-    -- },
-    -- {
-    --     "nvzone/typr",
-    -- },
+    {
+        "chenasraf/text-transform.nvim",
+        version = "*", -- or: tag = "stable"
+        dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+        config = function() 
+            require('text-transform').setup({
+                -- custom settings
+            })
+        end,
+    },
     -- {
     --     "jesses-code-adventures/bruno.nvim",
     --     -- Uses the pre-0.12 nvim-treesitter parser API.
@@ -66,14 +44,4 @@ return {
     --         vim.keymap.set('n', '<leader>pl', ':Pipeline open<CR>')
     --     end
     -- },
-    {
-        "chenasraf/text-transform.nvim",
-        version = "*", -- or: tag = "stable"
-        dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-        config = function() 
-            require('text-transform').setup({
-                -- custom settings
-            })
-        end,
-    },
 }
