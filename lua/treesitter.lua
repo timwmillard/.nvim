5e495cf93666cb750899d05e25344aeab9358c14
Tim Millard <timwmillard@gmail.com>
2026-05-31 19:30:07 +1000

    better treesitter support

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,7 +1,11 @@
 
 vim.api.nvim_create_autocmd("FileType", {
   callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    if ft == "" then
+      return
+    end
+
     pcall(vim.treesitter.start, args.buf)
   end,
 })
-
