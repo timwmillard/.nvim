ee523e4802cf808896fe2600afaf54b15b71c6bb
Tim Millard <timwmillard@gmail.com>
2026-06-01 09:28:56 +1000

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,17 @@
 -- File Search Fuzzy Finder
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
+        require('telescope').setup({
+            defaults = {
+                preview = {
+                    treesitter = false,
+                },
+            },
+        })
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pp', builtin.find_files, {})
 
